ca2a2e60bd1e7e6e66edfa1e3223941a2641781d
zakkoo <90413051+zakkoo@users.noreply.github.com>
2026-01-30 21:34:24 +0100

    keybindings

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -12,3 +12,17 @@ keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "References" })
 keymap.set("n", "<leader>td", "<cmd>lua require('dapui').toggle()<cr>", { desc = "Toggle DAP UI" })
 keymap.set("n", "<leader>tb", "<cmd>lua require('dap').toggle_breakpoint()<cr>", { desc = "Toggle breakpoint" })
 keymap.set("n", "<leader>tc", "<cmd>lua require('dap').continue()<cr>", { desc = "DAP continue" })
+
+
+
+
+ -- Step over
+  keymap.set("n", "<leader>so", "<cmd>lua require('dap').step_over()<cr>", { desc = "Step over" })
+  -- Step into
+  keymap.set("n", "<leader>si", "<cmd>lua require('dap').step_into()<cr>", { desc = "Step into" })
+  -- Step out
+  keymap.set("n", "<leader>su", "<cmd>lua require('dap').step_out()<cr>", { desc = "Step out" })
+  -- Terminate debugging
+  keymap.set("n", "<leader>tt", "<cmd>lua require('dap').terminate()<cr>", { desc = "Terminate debugging" })
+  -- Restart debugging
+  keymap.set("n", "<leader>tr", "<cmd>lua require('dap').restart()<cr>", { desc = "Restart debugging" })
