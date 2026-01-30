9d6bf0a1a1ea58d93dc2a1145e2ea935f40e9187
zakkoo <90413051+zakkoo@users.noreply.github.com>
2026-01-30 20:28:01 +0100

    nice looking now

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -6,7 +6,7 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme = "tokyonight",
+        theme = "nightfox",
         section_separators = "",
         component_separators = "",
       },
